c4832ab86e1a9a9a3c77672415d9d14e6b82ab07
SharanKumar <sharankumar.d@diggibyte.com>
2024-02-12 19:24:37 +0530

    handling null values

--- Practice_Pyspark/handling_null_value.py
+++ Practice_Pyspark/handling_null_value.py
@@ -11,20 +11,46 @@ data=[(11,'Sharan','Maths',88,'p'),
 columns=('id','name','subjects','marks','status')
 
 df=spark.createDataFrame(data,columns)
-df.show()
+# df.show()
+#
+# # It gives all rows having null values
+# df.filter(df.marks.isNull()).show()
+#
+# # It gives all rows which does not have null values
+# df.filter(df.marks.isNotNull()).show()
 
-# It gives all rows having null values
-df.filter(df.marks.isNull()).show()
+# # It gives rows which having null values in multiple columns
+# df.filter((df.marks.isNull()) & df.status.isNull()).show()
+#
+# # It checks either of the columns and gives the rows having null values
+# df.filter((df.marks.isNull()) | df.status.isNull()).show()
+#
+# # drop the row contains null values
+# df.na.drop().show()
+# # or
+# df.na.drop('any').show()
 
-# It gives all rows which does not have null values
-df.filter(df.marks.isNotNull()).show()
+# Alternate syntax for drop a row contains null values
+# df.dropna().show()
 
-# It gives rows which having null values in multiple columns
-df.filter((df.marks.isNull()) & df.status.isNull()).show()
+# # remove the row having null values in all the column
+# df.na.drop('all').show()
 
-df.filter((df.marks.isNull()) | df.status.isNull()).show()
+# Removes all the rows having null in a particular column
+# df.na.drop(subset='marks').show()
+#
+# # Removes all the rows having null in multiple columns column
+# df.na.drop(subset=['marks','status']).show()
+#
+# # It replaces the null values with 0 only in integer columns 'marks'
+# df.na.fill(value=0).show()
+#
+# # It replaces the null values with 'NA' only in string columns 'name','subject','Status'
+# df.na.fill(value='NA').show()
 
-# drop the row contain null values
-df.na.drop().show()
-# or
-df.na.drop('any').show()
+# it replaces the null value based on the particular column/multiple columns as we mention in subset
+df.na.fill(value=0,subset=['marks']).show()
+df.na.fill(value=0,subset=['marks','id']).show()
+
+# replacing the null values in all columns with any other values
+df.na.fill({'marks':0, 'status':'NA', 'subjects':'Hindi','id':16,'name':'no_name'}).show()
