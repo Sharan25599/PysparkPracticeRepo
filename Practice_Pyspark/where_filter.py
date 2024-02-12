5ac56f710b0bf9d839898285ed3e3fa5a6aef692
SharanKumar <sharankumar.d@diggibyte.com>
2024-02-12 19:23:59 +0530

    practiced on filter function

--- Practice_Pyspark/where_filter.py
+++ Practice_Pyspark/where_filter.py
@@ -15,3 +15,40 @@ df_fiter.show()
 df_filter1=df.where((df.age >= 25) & (df.gender == 'M'))
 df_filter1.show()
 
+
+# using Filter()
+
+data=[(10,'Rahul',100,'M',2000),(20,'Barath',101,'M',4000),(30,'Balaji',102,'M',5000),(40,'Pooja',103,'F',3000)]
+schema=('id','name','emp_dept_id','gender','salary')
+
+df = spark.createDataFrame(data,schema)
+
+# filter the salary column in which sal is greater then and equal to 2500
+df.filter(df.salary >=2500).show()
+
+#filter the gender where gender equal to female
+df.filter(df.gender == 'F').show()
+
+# filter based upon gender is male and salary is 4000
+df.filter((df.gender=='M') &(df.salary== 4000)).show()
+
+# checks whether the salary column is null
+df.filter(df.salary.isNull()).show()
+
+# checks whether the salary column is notnull
+df.filter(df.salary.isNotNull()).show()
+
+# filter name end with character 'l' using like()
+df.filter(df.name.like('%l')).show()
+
+# filter name ends with characters 'ja' using endswith()
+df.filter(df.name.endswith('ja')).show()
+
+# filter name starts with characters 'Ba' using startswith()
+df.filter(df.name.startswith('Ba')).show()
+
+# we can check the value is present in the table using isin()
+df.filter(df.id.isin(10,30)).show()
+
+
+
